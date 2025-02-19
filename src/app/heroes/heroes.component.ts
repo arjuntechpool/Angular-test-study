99a11bd1d34be5f04aebcc6ddbe00b17ca7e4988
arjuntechpool <arjun@techpool.co.in>
2025-02-19 11:14:49 +0530

    Updated Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
-import { CommonModule } from '@angular/common';
+// import { Hero } from '../hero';
+import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [NgFor, CommonModule, NgIf,FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
+// export class HeroesComponent {
+//   hero: Hero = {
+//     id: 1,
+//     name: 'Windstorm'
+//   };
+// }
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
